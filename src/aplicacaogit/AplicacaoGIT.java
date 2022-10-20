1543262c5180f28b388bd204b43417fd26a66cbf
Gabriel Alves Pereira <yuurei.ncw@gmail.com>
2022-10-19 21:45:45 -0300

    GIT

---
+++ src/aplicacaogit/AplicacaoGIT.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacaogit;
+
+/**
+ *
+ * @author Geral
+ */
+public class AplicacaoGIT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Nosso primeiro projeto com gerenciamento de Versão");
+        System.out.println("Peixe esquilo");
+    }
+    
+}
